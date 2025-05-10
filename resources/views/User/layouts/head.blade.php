df56ea6e5646056dde39da4016f436d300835a6a
WayanDev <wayantrk@gmail.com>
2025-05-10 09:21:49 +0700

    update

--- resources/views/User/layouts/head.blade.php
+++ resources/views/User/layouts/head.blade.php
@@ -4,6 +4,7 @@
     <title>@yield('title', 'Beacon Engineering')</title>
     <meta name="description" content="Beacon Engineering">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <link rel="shortcut icon" type="image/x-icon" href="asset/img/favicon.png"> --}}
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/dist/img/title.ico') }}">
     <!-- Place favicon.ico in the root directory -->
